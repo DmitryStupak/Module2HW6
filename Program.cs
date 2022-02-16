785cc774cf1c851f5b7d6130d56b882a8eadcb95
DmitryStupak <dimast_ukr@outlook.com>
2022-02-16 16:34:39 +0200

    1

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace Module2HW6
             Console.WriteLine("Сортировка машин по расходу топлива");
             taxiDepot.GetCars().Sort();
             taxiDepot.GetCars().PrintList();
+            ((IFord)taxiDepot.GetCars()[0]).WhatIAm();
         }
     }
 }
